d9b10f9559320dd46d3a32bddea84d158f62611b
ariya sobuj <sobujsorkar123@gmail.com>
2021-09-30 15:18:57 +0600

    mobile navbar deign

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,33 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav__menu'),
+      navToggel = document.getElementById('nav__toggle'),
+      navClose = document.querySelector('.close'),
+      navItem= document.querySelectorAll('.nav__item');
+
+
+
+
+
+if(navToggel) {
+    navToggel.addEventListener('click', function () {
+        navMenu.classList.add('show__menu')
+    })
+}
+if(navClose) {
+    navClose.addEventListener('click' , function () {
+        navMenu.classList.remove('show__menu')
+    })
+}
+
+navItem.forEach(item => {
+    item.addEventListener('click', function ( ){
+        navMenu.classList.remove('show__menu')
+    })
+})
+
+
+
+
 
 
 /*===== MENU SHOW =====*/
